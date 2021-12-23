72737bcdca74094d5d4fe1c6f3bb08eb0016bc05
Roope Keto-Tokoi <roope.keto-tokoi@outlook.com>
2021-12-23 21:35:29 +0200

    put far plane infinitely far away

---
+++ Assets/MiscellaneousScripts/NoFarPlane.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+//gets the camera component and moves the far plane infinitely far
+[RequireComponent(typeof(Camera))]
+public class NoFarPlane : MonoBehaviour
+{
+    void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+
+        Matrix4x4 matrix = cam.projectionMatrix;
+
+        //see http://www.terathon.com/gdc07_lengyel.pdf
+        float epsilon = (float)System.Math.Pow(2, -21);//according to above site, 2^-22 is the minimum value
+        matrix[2, 2] = epsilon - 1;
+        matrix[2, 3] = (epsilon - 2) * cam.nearClipPlane;
+        matrix[3, 2] = -1;
+
+        cam.projectionMatrix = matrix;
+    }
+}
